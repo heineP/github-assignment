379ffd1eae6cd49123f508debbf43040f1e49aa7
heineP <heinep25@gmail.com>
2024-01-19 08:33:15 +0100

    Made some comments

--- github-assignment/Program.cs
+++ github-assignment/Program.cs
@@ -1,14 +1,14 @@
 ﻿namespace github_assignment;
 
 class Program
-{
+{    // hello  :)
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
         Console.WriteLine("My name is Heine");
         CountTo(3);
     }
-
+    // Counts from 1 to parameter
     static void CountTo(int num)
     {
         for (int i = 1; i <= num; i++)
